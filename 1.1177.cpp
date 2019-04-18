f44d28df8c7e058c66350d54638338f696b4f2ed
happyZYM <happyzymak@qq.com>
2019-04-18 20:59:20 +0800

    Backup : 2019-04-18 20:59:20

--- 1.1177.cpp
+++ 1.1177.cpp
@@ -1,36 +1,18 @@
 #include<cstdio>
 #include<algorithm>
-using std::swap;
-int n,a[100005];
-void sort(int L,int R)
-{
-	if(L>=R) return;
-	int k=a[(L+R)>>1],p1=L,p2=R;
-	do
-	{
-		while(a[p1]<k) p1++;
-		while(a[p2]>k) p2--;
-		if(p1<=p2)
-		{
-			swap(a[p1],a[p2]);
-			p1++;
-			p2--;
-		}
-	}
-	while(p1<=p2);
-	sort(L,p2);
-	sort(p1,R);
-}
+#include<cstdlib>
+using namespace std;
+const int maxn=100005;
+int n,a[maxn];
 int main()
 {
-	scanf("%d",&n);
-	for(int i=0;i<n;i++) scanf("%d",a+i);
-	sort(0,n-1);
-	for(int i=0;i<n;i++)
-	{
-		if(i) printf(" ");
-		printf("%d",a[i]);
-	}
-	printf("\n");
-	return 0;
+#ifdef local
+    freopen("pro.in","r",stdin);
+#endif
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    sort(a,a+n);
+    for(int i=0;i<n;i++) printf("%d ",a[i]); puts("");
+    return 0;
 }
+
