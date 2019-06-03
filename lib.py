809b4c957d5c9d1e24504177a299ce048a5e765b
happyZYM <happyzymak@qq.com>
2019-06-03 20:46:37 +0800

    Backup : 2019-06-03 20:46:37

--- lib.py
+++ lib.py
@@ -50,7 +50,10 @@ class Input:
         self.alloc()
         if self.eof:
             return ''
-        res=self.io_line_buf[self.io_line_buf_cnt:-1]
+        if self.io_line_buf[-1:]=='\n':
+            res=self.io_line_buf[self.io_line_buf_cnt:-1]
+        else:
+            res=self.io_line_buf[self.io_line_buf_cnt:]
         self.io_line_buf_cnt=len(self.io_line_buf)
         return res
     
@@ -141,6 +144,53 @@ def gcd(a,b):
 
 def lcm(a,b):
     return int(a/gcd(a,b)*b)
+
+def yf(a,b):
+	c=gcd(a,b)
+	a=a/c
+	b=b/c
+	return [a,b]
+
+#自适应辛普森积分
+'''c++版
+#include<cstdio>
+#include<cmath>
+double a;
+inline double f(double x) { return pow(x,a/x-x); }
+inline double simpson(double a,double b) { return (f(a)+4*f((a+b)/2)+f(b))*(b-a)/6; }
+double asr(double a,double b,double eps,double A)
+{
+    double c=(a+b)/2;
+    double L=simpson(a,c),R=simpson(c,b);
+    if(fabs(L+R-A)<=15*eps) return L+R+(L+R-A)/15;
+    return asr(a,c,eps/2,L)+asr(c,b,eps/2,R);
+}
+double asr(double a,double b,double eps) { return asr(a,b,eps,simpson(a,b)); }
+int main()
+{
+#ifdef local
+    freopen("pro.in","r",stdin);
+#endif
+    scanf("%lf",&a);
+    const double eps=1e-8;
+    if(a<0) printf("orz\n");
+    else printf("%.5lf\n",asr(eps,20,eps));
+    return 0;
+}
+'''
+def simpson(f,a,b):
+	return (f(a)+4*f((a+b)/2.0)+f(b))*(b-a)/6.0
+
+def asr(f,a,b,eps,A):
+	c=(a+b)/2.0
+	L=simpson(f,a,c)
+	R=simpson(f,c,b)
+	if abs(L+R-A)<=15*eps:
+		return L+R+(L+R-A)/15.0
+	return asr(f,a,c,eps/2.0,L)+asr(f,c,b,eps/2.0,R)
+
+def sps(f,a,b,eps):
+	return asr(f,a,b,eps,simpson(f,a,b))
 
 # Data Struct
 def Array(*idx,dft=0):
