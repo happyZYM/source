30544c8988fbe3727464da099341f65ad1fe75a0
happyZYM <happyzymak@qq.com>
2019-06-22 21:37:22 +0800

    Backup : 2019-06-22 21:37:22

---
+++ 1.4551.cpp
@@ -0,0 +1,56 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+const int maxn=100005;
+const int maxmem=maxn*31;
+int n,d[maxn],E[maxmem][2],cnt;
+struct Edge { int v,w; Edge *nxt; };
+Edge mem[maxn*2],*G[maxn],*ecnt;
+inline void AddEdge(int u,int v,int w) { ecnt->v=v; ecnt->w=w; ecnt->nxt=G[u]; G[u]=ecnt++; }
+void dfs(int u,int fa,int val)
+{
+	d[u]=val;
+	for(Edge *it=G[u];it;it=it->nxt) if(it->v!=fa) dfs(it->v,u,val^it->w);
+}
+inline void insert(int x)
+{
+	int p=0;
+	for(int i=30;i>=0;i--)
+	{
+		int now=(x>>i)&1;
+		if(!E[p][now]) E[p][now]=++cnt;
+		p=E[p][now];
+	}
+}
+inline int query(int x)
+{
+	int p=0,res=0,w=0;
+	for(int i=30;i>=0;i--)
+	{
+		int now=(x>>i)&1;
+		if(E[p][now^1]) { p=E[p][now^1]; res+=1<<i; }
+		else p=E[p][now];
+	}
+	return res;
+}
+int main()
+{
+#ifdef local
+	freopen("pro.in","r",stdin);
+#endif
+	scanf("%d",&n);
+	ecnt=mem; memset(G,0,sizeof(G));
+	memset(E,0,sizeof(E)); cnt=0;
+	for(int i=0;i<n-1;i++)
+	{
+		int u,v,w; scanf("%d%d%d",&u,&v,&w);
+		AddEdge(u,v,w); AddEdge(v,u,w);
+	}
+	dfs(1,-1,0);
+	int res=0;
+	for(int i=1;i<=n;i++) insert(d[i]);
+	for(int i=1;i<=n;i++) res=max(res,query(d[i]));
+	printf("%d\n",res);
+	return 0;
+}
